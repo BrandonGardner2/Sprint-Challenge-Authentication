15cd39d2b0d73c6236045288a2c1c262491b68c4
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-05 10:21:49 -0500

    all routes working correctly with jwt

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { authenticate } = require("../auth/authenticate");
 const db = require("../database/dbConfig");
@@ -11,17 +12,22 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  const { username, password } = req.body;
+  const user = req.body;
 
-  if (username && password) {
-    password = bcrypt.hashSync(password, 8);
+  if (user.username && user.password) {
+    user.password = bcrypt.hashSync(user.password, 8);
 
     db("users")
-      .insert({ username, password })
-      .then(res => {
-        res.status(200).json(res);
+      .insert(user)
+      .then(([id]) => {
+        res.status(200).json(id);
       })
-      .catch(err => console.error("REGISTER ERROR", err));
+      .catch(err => {
+        console.error("REGISTER ERROR", err);
+        res
+          .status(500)
+          .json({ message: "Something went wrong during registration." });
+      });
   } else {
     res
       .status(400)
@@ -30,7 +36,33 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  const login = req.body;
+
+  if (login.username && login.password) {
+    db("users")
+      .where({ username: login.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(login.password, user.password)) {
+          const token = generateToken(user);
+
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token
+          });
+        } else {
+          res.status(401).json({ message: "Invalid credentials." });
+        }
+      })
+      .catch(err => {
+        console.error("LOGIN ERROR", err);
+        res.status(500).json({ message: "Oops, we had an issue logging in." });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide your username and password." });
+  }
 }
 
 function getJokes(req, res) {
@@ -46,4 +78,21 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const secret =
+    process.env.JWT_SECRET ||
+    "add a .env file to root of project with the JWT_SECRET variable";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
 }
